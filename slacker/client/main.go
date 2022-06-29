4586b3f5ac52559de22c8815cb290f799d2ca519
Suriya Ganesh <suriyaganesh097@gmail.com>
2022-06-30 05:18:29 +0530

    Basic commit

---
+++ slacker/client/main.go
@@ -0,0 +1,11 @@
+package main
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	// Set up a connection to the server
+
+}
---
+++ slacker/go.mod
@@ -0,0 +1,8 @@
+module slacker
+
+go 1.16
+
+require (
+	google.golang.org/grpc v1.47.0
+	google.golang.org/grpc/examples v0.0.0-20220628173744-c9b16c884c97
+)
